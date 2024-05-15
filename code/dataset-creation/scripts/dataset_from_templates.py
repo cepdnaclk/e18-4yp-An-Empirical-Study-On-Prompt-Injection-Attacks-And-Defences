12c1e0cd096f2d15e4c1752781df97a4a4ab392a
denuwan55 <ldweerarathne5555@gmail.com>
2024-05-15 21:52:56 +0530

    groq inference code added

--- code/dataset-creation/scripts/dataset_from_templates.py
+++ code/dataset-creation/scripts/dataset_from_templates.py
@@ -1,47 +1,58 @@
 import csv
 from typing import Dict, List
 
+
 def load_csv(file_path: str) -> List[Dict[str, str]]:
     """
     Load a CSV file into a list of dictionaries.
     """
     data = []
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
     return data
 
-def generate_combinations(templates: List[Dict[str, str]], prompts: List[Dict[str, str]]) -> List[Dict[str, str]]:
+
+def generate_combinations(
+    templates: List[Dict[str, str]], prompts: List[Dict[str, str]]
+) -> List[Dict[str, str]]:
     """
     Generate all possible combinations of templates and prompts.
     """
     combinations = []
     for template in templates:
         for prompt in prompts:
-            result = template['template'].replace('[INSERT PROMPT HERE]', prompt['prompt'])
-            combinations.append({
-                'template': template['template'],
-                'prompt': prompt['prompt'],
-                'result': result
-            })
+            result = template["template"].replace(
+                "[INSERT PROMPT HERE]", prompt["prompt"]
+            )
+            combinations.append(
+                {
+                    "template": template["template"],
+                    "prompt": prompt["prompt"],
+                    "result": result,
+                }
+            )
     return combinations
+
 
 def write_csv(file_path: str, data: List[Dict[str, str]], fieldnames: List[str]):
     """
     Write data to a CSV file.
     """
-    with open(file_path, 'w', newline='') as file:
+    with open(file_path, "w", newline="") as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(data)
 
-def main():
-    templates = load_csv('../data-from-internet/jbTemplates.csv')
-    prompts = load_csv('../data-from-internet/questions.csv')
-    combinations = generate_combinations(templates, prompts)
-    fieldnames = ['template', 'prompt', 'result']
-    write_csv('output.csv', combinations, fieldnames)
 
-if __name__ == '__main__':
+def main():
+    templates = load_csv("../data-from-internet/jbTemplates.csv")
+    prompts = load_csv("../data-from-internet/questions.csv")
+    combinations = generate_combinations(templates, prompts)
+    fieldnames = ["template", "prompt", "result"]
+    write_csv("output.csv", combinations, fieldnames)
+
+
+if __name__ == "__main__":
     main()
---
+++ code/llm-inferences/groq-platform/inference.py
@@ -0,0 +1,94 @@
+"""
+Initial code was take from official documentation of Groq (https://console.groq.com/docs/text-chat#performing-a-basic-chat-completion)
+"""
+
+import csv
+from groq import Groq
+from dotenv import load_dotenv
+from typing import Dict, List
+
+# Load environment variables from .env file
+load_dotenv()
+
+client = Groq()
+
+
+def write_csv(file_path: str, data: List[Dict[str, str]], fieldnames: List[str]):
+    """
+    Write data to a CSV file.
+    """
+    with open(file_path, "w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def load_csv(file_path: str) -> List[Dict[str, str]]:
+    """
+    Load a CSV file into a list of dictionaries.
+    """
+    data = []
+    try:
+        with open(file_path, "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                data.append(row)
+    except FileNotFoundError:
+        # Create a new file
+        print(f"File at {file_path} not found. Creating a new file as given.")
+    return data
+
+
+def get_response(
+    client: Groq = Groq(),
+    system_content: str = "You are a helpful assistant",
+    user_content: str = "Say Hello",
+    model: str = "llama3-8b-8192",
+) -> str:
+    chat_completion = client.chat.completions.create(
+        #
+        # Required parameters
+        #
+        messages=[
+            # Set an optional system message. This sets the behavior of the
+            # assistant and can be used to provide specific instructions for
+            # how it should behave throughout the conversation.
+            {
+                "role": "system",
+                "content": system_content,
+            },
+            # Set a user message for the assistant to respond to.
+            {
+                "role": "user",
+                "content": user_content,
+            },
+        ],
+        # The language model which will generate the completion.
+        model=model,
+        #
+        # Optional parameters
+        #
+        # Controls randomness: lowering results in less random completions.
+        # As the temperature approaches zero, the model will become deterministic
+        # and repetitive.
+        temperature=0.5,
+        # The maximum number of tokens to generate. Requests can use up to
+        # 32,768 tokens shared between prompt and completion.
+        max_tokens=1024,
+        # Controls diversity via nucleus sampling: 0.5 means half of all
+        # likelihood-weighted options are considered.
+        top_p=1,
+        # A stop sequence is a predefined or user-specified text string that
+        # signals an AI to stop generating content, ensuring its responses
+        # remain focused and concise. Examples include punctuation marks and
+        # markers like "[end]".
+        stop=None,
+        # If set, partial message deltas will be sent.
+        stream=False,
+    )
+    return chat_completion.choices[0].message.content
+
+
+file = load_csv("ABC.csv")
+fieldnames = ["llm", "prompt", "llm_answer"]
+data = []
